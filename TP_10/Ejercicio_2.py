3a8ad5f93224890da2f44c2577f4749f8960e57b
Ldispenza272 <l.dispenza@alumno.um.edu.ar>
2026-08-18 19:34:36 -0300

    ej 2

--- TP_10/Ejercicio_2.py
+++ TP_10/Ejercicio_2.py
@@ -1,3 +1,4 @@
 num1 = int(input("Ingrese un número "))
 num2 = int(input("Ingrese el segundo número "))
 print("El resultado de la multiplicación es ", num1 * num2)
+print("El resultado de la suma es ", num1 + num2)
