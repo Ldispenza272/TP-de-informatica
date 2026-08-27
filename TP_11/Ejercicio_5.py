c7eed23d173be2b7e1c9f3189cc7de4a7cdb861f
Ldispenza272 <l.dispenza@alumno.um.edu.ar>
2026-08-27 17:59:57 -0300

    ej 5 terminado

--- TP_11/Ejercicio_5.py
+++ TP_11/Ejercicio_5.py
@@ -1,8 +1,6 @@
-import math
-
 Altura = float(input("Ingrese su altura en metros: "))
 Peso= float(input("Ingrese su peso en kilogramos: "))
-IMC =math.trunc(Peso / (Altura ** 2))
+IMC =  round((Peso / (Altura ** 2)), 3)
 if IMC < 18.5:
     print("Su IMC es: ", IMC, " - Bajo peso")
 elif IMC >= 18.5 and IMC < 24.9:
